de72c4450ee3136a4b1964446b2b1465f998ca00
Junior <paulo.cesar.junior@accenture.com>
2019-09-01 18:45:57 -0300

    login page finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'my-phone', loadChildren: () => import('./my-phone/my-phone.module').then( h => h.MyPhonePageModule)},
+  { path: 'login', loadChildren: () => import('./login/login.module').then( l => l.LoginPageModule)},
 ];
 
 @NgModule({
